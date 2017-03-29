1493af134ced5954d7efeada76427387cde108be
LL <thatmelon@gmail.com>
2017-03-29 13:25:56 +0900

    week 2 lab work

--- top level/top.v
+++ top level/top.v
@@ -27,7 +27,15 @@ module top(
 	  output [6:0] HEX2,
 	  output [6:0] HEX3,
 	  output [6:0] HEX4,
-	  output [6:0] HEX5
+	  output [6:0] HEX5,
+	  output 	VGA_CLK,   						//	VGA Clock
+	  output 	VGA_HS,							//	VGA H_SYNC
+	  output 	VGA_VS,							//	VGA V_SYNC
+	  output 	VGA_BLANK_N,						//	VGA BLANK
+	  output 	VGA_SYNC_N,						//	VGA SYNC
+	  output 	VGA_R,   						//	VGA Red[9:0]
+	  output 	VGA_G,	 						//	VGA Green[9:0]
+	  output 	VGA_B   						//	VGA Blue[9:0]
   );
 
   wire [31:0] note_freq;
@@ -118,7 +126,54 @@ hex_decoder h5(.hex_digit({2'b00,octave}), .segments(HEX5));
   			   .GPIO_1(GPIO_1)
   				);
 
+				
+				
+				
+			
+	// Create the colour, x, y and writeEn wires that are inputs to the controller.
+	wire [2:0] colour;
+	wire [7:0] x;
+	wire [6:0] y;
+	wire writeEn;
 
+	// Create an Instance of a VGA controller - there can be only one!
+	// Define the number of colours as well as the initial background
+	// image file (.MIF) for the controller.
+	vga_adapter VGA(
+			.resetn(KEY[0]),
+			.clock(CLOCK_50),
+			.colour(colour),
+			.x(x),
+			.y(y),
+			.plot(writeEn),
+			/* Signals for the DAC to drive the monitor. */
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK_N),
+			.VGA_SYNC(VGA_SYNC_N),
+			.VGA_CLK(VGA_CLK));
+		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.MONOCHROME = "FALSE";
+		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+		defparam VGA.BACKGROUND_IMAGE = "vga_adapter/black.mif";
+			
+
+			
+	vga_data vgad(
+					.note(note), 
+					.octave(octave), 
+					.clk(CLOCK_50), 
+					.reset(KEY[0]),
+					.x(0), //from coord picker/datapath
+					.y(0), //from coord picker/datapath
+					.x_out(x), 
+					.y_out(y), 
+					.writeEn(writeEn),
+					.colour(colour)
+					);
 endmodule
 
 module hex_decoder(hex_digit, segments);
